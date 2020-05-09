c25ff1169c8080121eee7086408b4750d3ba1bc5
Vitor Lúcio <lucivitor@gmail.com>
2020-05-08 23:03:06 -0300

    Lista ate a opcao 9

--- Lista_de_listas.py
+++ Lista_de_listas.py
@@ -1,5 +1,5 @@
-alunos = []
-notas = []
+alunos = ["vitor","ana","lucas","vitinho"]
+notas = [[7,8,9],[10,9,8],[7,5,6],[8,9,8]]
 def adicionar_nota():
     nome = input("Qual o nome do Aluno: ")
     nota = input("Qual a nota do Aluno: ")
@@ -60,11 +60,54 @@ def editar_nota_do_aluno():
         notas[posicao_aluno][nota - 1] = nova_nota
         return
 
+def buscar_aluno_por_nome():
+    nome = input("Qual o nome de aluno que você deseja buscar:")
+    contador = 0
+
+    for nome_do_aluno in alunos:
+        if nome_do_aluno.upper().startswith(nome.upper()):
+            contador = contador + 1
+            indice = alunos.index(nome_do_aluno)
+            media = sum(notas[indice]) / 3
+            print("%d. %s. Notas: %s. Média: %.2f" % (contador,nome_do_aluno, ', '.join(map(str, notas[indice])),media))
+            #' , ' para separar a lista de notas com virgula / .join para juntar os itens da lista e map para transformar de inteiro para str
+
+def calcular_media_da_turma():
+    total_notas = []
+    for nota in notas:
+        total_notas.append(sum(nota)/3)
+    media = sum(total_notas) / len(notas)
+    print("A média da turma é: %.2f" % (media))
+
+def exibir_melhor_aluno():
+    melhor_media = 0
+    index = 0
+    contador = 0
+    for nota in notas:
+       media = sum(nota) / len(nota)
+       if media > melhor_media:
+           melhor_media = media
+           index = contador
+
+       contador = contador + 1
+    print("%s. Notas: %s. Média: %.2f" % (alunos[index], ', '.join(map(str, notas[index])), melhor_media))
+
+def exibir_alunos_em_ordem_afabetica():
+    alunos.sort()
+
+
+
+
+
+
+
+
+
 
 
 
 while True :
-    opcao = int(input('O que você deseja fazer: \n\t1) Adicionar Aluno\n\t2) Adicionar Nota\n\t3) Remover Aluno\n\t4) Remover Nota\n\t5) Editar Nome do Aluno\n\t6) Editar Nota do Aluno\n\t7) Buscar Aluno Por Nome\n\t8) Calcular Média da Turma\n\t9) Exibir Melhor Aluno\n\t10) Exibir Alunos Em Ordem Afabética\n\t11) Exibir Aluno Por Ordenados Por Nota\n\t12) Exibir Aluno Aprovados Por Média (>=7)\n\t13) Exibir Alunos Na Final (>=5)\n\t14) Exibir Alunos Reprovados (<5)\n\t15) Encerrar o Programa\n Digite a Sua Opção :'))
+    opcao = int(input('O que você deseja fazer: \n\t1) Adicionar Aluno\n\t2) Adicionar Nota\n\t3) Remover Aluno\n\t4) Remover Nota\n\t5) Editar Nome do Aluno\n\t6) Editar Nota do Aluno\n\t7) Buscar Aluno Por Nome\n\t8) Calcular Média da Turma\n\t9) Exibir Melhor Aluno\n\t10) Exibir Alunos Em Ordem Afabética\n\t11) Exibir Alunos Ordenados Por Nota\n\t12) Exibir Aluno Aprovados Por Média (>=7)\n\t13) Exibir Alunos Na Final (>=5)\n\t14) Exibir Alunos Reprovados (<5)\n\t15) Encerrar o Programa\n Digite a Sua Opção :'))
     if opcao == 15:
         print("Bye.")
         break
@@ -86,9 +129,27 @@ while True :
     elif opcao == 6:
         editar_nota_do_aluno()
     elif opcao == 7:
+        buscar_aluno_por_nome()
+        #ok
+    elif opcao == 8:
+        calcular_media_da_turma()
+        #ok
+    elif opcao == 9:
+        exibir_melhor_aluno()
+        #ok
+    elif opcao == 10:
+        exibir_alunos_em_ordem_afabetica()
+    elif opcao == 11:
+        exibir_alunos_ordenados_por_nota()
+    elif opcao == 12:
+        exibir_aluno_aprovados_por_media()
+    elif opcao == 13:
+        exibir_alunos_na_final()
+    else:
+        exibir_alunos_reprovados()
 
-    print(alunos)
-    print(notas)
+
+
 
 
 
